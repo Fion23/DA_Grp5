40ed4a623b9254dc003fed0e58cf43693da47839
Fion23 <87792678+Fion23@users.noreply.github.com>
2021-08-19 10:14:22 +0800

    Add files via upload

---
+++ project.py
@@ -0,0 +1,33 @@
+#lab 10b
+import requests
+# Importing request library
+url = 'http://www.wikipedia.org’
+# Set the target webpage
+r = requests.get(url)
+# get request
+print(r.text)
+# This will get the full page of get request
+
+print("Status code:")
+# This will show status code:
+print("\t *", r.status_code)
+# This will get the status code, 200 which also means OK
+
+h = requests.head(url)
+# This will just get just the headers
+print("Header:")
+# This will show Header:
+print("**********")
+for x in h.headers:
+# To print line by line
+	print("\t ", x, ":", h.headers[x])
+print("**********")
+
+headers = {
+# This will modify the headers user-agent
+	'User-Agent' : ‘Mobile’
+}
+url2 = 'http://httpbin.org/headers'
+# Test it on an alternate site
+rh = requests.get(url2, headers=headers)
+print(rh.text)
